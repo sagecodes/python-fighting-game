c661fa1b05a901e0f92be314d90ab4db7a308248
Sage Elliott <wildtroll@gmail.com>
2015-08-03 06:19:40 -0700

    create character basics - input for weapon selection - input for name

---
+++ character.py
@@ -0,0 +1,23 @@
+class Character:
+    experience = 0
+    hit_points = 0
+
+    def get_weapon(self):
+        weapon_choice = input("Weapon ([S]word, [A]xe, [B]ow): ").lower()
+
+        if weapon_choice in 'sab':
+            if weapon_choice == 's':
+                return 'sword'
+            elif weapon_choice == 'a':
+                return 'axe'
+            else:
+                return 'bow'
+        else:
+            self.get_weapon()
+
+    def __init__(self, **kwargs):
+        self.name = input("Name: ")
+        self.weapon = self.get_weapon()
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
