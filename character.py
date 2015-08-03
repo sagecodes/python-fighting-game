8592bd84782aa15716a5887c14aef67de3f1866c
Sage Elliott <wildtroll@gmail.com>
2015-08-03 06:40:57 -0700

    redefine attack limit for character and add bonuses for sword or axe

--- character.py
+++ character.py
@@ -1,8 +1,19 @@
+import random
 from combat import Combat
 
 class Character(Combat):
+    attack_limit = 10
     experience = 0
     hit_points = 0
+
+    def attack(self):
+        roll = random.randint(1, self.attack_limit)
+        if self.weapon == 'sword':
+            roll += 1
+        elif self.weapon == 'axe':
+            roll += 2
+        return roll > 4
+
 
     def get_weapon(self):
         weapon_choice = input("Weapon ([S]word, [A]xe, [B]ow): ").lower()
